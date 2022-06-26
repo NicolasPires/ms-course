c32fc77e1e056a60205bf73894924a5b6ed14b89
nicolas.pires <nicolask.pires@gmail.com>
2022-06-25 23:32:53 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/main/java/com/nksolucoes/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.nksolucoes.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
